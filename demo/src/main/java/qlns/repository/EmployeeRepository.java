56325cfa92530f2970ba3b9551db85d50d6dd1b2
dungnguyen-art <dungtpa2@gmail.com>
2022-06-02 01:20:44 +0700

    v1

---
+++ demo/src/main/java/qlns/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package qlns.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import qlns.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
